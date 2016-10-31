128cf6bf59852037fd851c24a90b2ff1443e6397
Ryan Thames <rthames62@gmail.com>
2016-10-31 14:31:38 -0500

    everything works and is awesome

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express")
     , app     = express()
+    , server  = require("http").createServer(app)
+    , io      = require("socket.io").listen(server)
     , port    = 8888;
 
 app.use(express.static(`${__dirname}/public`));
 
-app.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
+server.listen(process.env.PORT || port, () => console.log(`Listening on port ${port}`));
 
 
 // TODO
 // SOCKET STUFF WILL GO HERE
+
+const connections = [];
+const users       = [];
+
+io.sockets.on("connection", socket => {
+  connections.push(socket);
+  console.log(`Connected: ${connections.length} socket(s) on ${port}`);
+
+  socket.on("disconnect", data => {
+    connections.splice(connections.indexOf(data), 1);
+    console.log(`Disconnected: ${connections.length} socket(s) on ${port}`);
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsers();
+  })
+
+  socket.on("new user", data => {
+    socket.username = data;
+    users.push(data);
+    updateUsers();
+  });
+
+  socket.on("send message", data => {
+    io.sockets.emit("get message", {user: socket.username, msg: data});
+  });
+
+  function updateUsers(){
+    io.sockets.emit("update users", users);
+  }
+
+  
+
+});
--- public/app.js
+++ public/app.js
@@ -2,22 +2,36 @@ angular.module("socketApp", [])
 .controller("mainCtrl", function($scope){
 
     //TODO SOCKET THINGSSSSSZZ
+    const socket = io.connect();
 
     $scope.showLogin = true;
     $scope.users     = [];
     $scope.messages  = [];
 
+
     $scope.newUser = username => {
         if(username){
             $scope.showLogin = !$scope.showLogin;
             //TODO
+            socket.emit("new user", username);
         }
     }
 
     $scope.sendMessage = message => {
         if(message){
             //TODO
+            socket.emit("send message", message);
         }
     }
+
+    socket.on("update users", data => {
+      $scope.users = data;
+      $scope.$apply();
+    });
+
+    socket.on("get message", data => {
+      $scope.messages.push(data);
+      $scope.$apply();
+    });
 
 });
